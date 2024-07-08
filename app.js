f89369943e4a2c2ae065b4effb48c07d63f73560
Guru Prakash Gupta <080bct032@ioepc.edu.np>
2024-07-08 09:44:17 +0545

    2nd day addded

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 const express =require("express")
+const connectToDb =require("./database/databaseConnection")
+const Blog = require("./model/blogModel")
 const app =express()
+connectToDb()
+app.use(express.json())
+app.use(express.urlencoded({extended : true}))
+
+app.set('view engine','ejs')
+
 app.listen(3000,()=>{ 
     console.log("Nodejs has started",+3000)})
 app.get("/",(req,res)=>{
@@ -16,8 +24,28 @@ app.get("/ejs",(req,res)=>{
     res.render("about.ejs")
     
 })
+app.get("/blog",(req,res)=>{
+    res.render("createblog.ejs")
+})
 app.get("/contact",(req,res)=>{
     const data="access workshop"
     res.render("contact.ejs",{data})
     
+})
+app.post("/createblog",async (req,res)=>{
+    //console.log(req.body)
+    //const title =req.body.tile
+    //const subtitle=req.body.subtitle
+    //const decription=req.body.description
+    //const subtitle=req.body.subtitle
+    //const image=req.body.image
+    const {title,subtitle,description}=req.body
+    console.log(title,subtitle,description)
+    await Blog.create({
+        title,
+        subtitle,
+        description
+    })
+    
+    res.send("post hitted")
 })
---
+++ database/databaseConnection.js
@@ -0,0 +1,8 @@
+const mongoose=require("mongoose")
+
+async function connectToDb(){
+await mongoose.connect("mongodb+srv://gurugupta22:guru1127@cluster0.0bkbuqf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"),
+console.log("database connected")
+}
+
+module.exports = connectToDb
