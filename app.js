3c04a0ecc10fc8611c7ec71081ec1d18a931f6c2
Guru Prakash Gupta <080bct032@ioepc.edu.np>
2024-07-14 04:01:40 +0545

    image edited

--- app.js
+++ app.js
@@ -1,155 +1,160 @@
-
 require("dotenv").config()
 const express = require("express")
 const connectToDb = require("./database/databaseConnection")
 const Blog = require("./model/blogModel")
 const bcrypt = require('bcrypt')
+const multer = require('multer')
+const path = require('path')
+
 const app = express() 
-// const multer = require("./middleware/multerConfig").multer
-// const storage = require("./middleware/multerConfig").storage
 const cookieParser = require('cookie-parser')
 
 app.use(cookieParser())
 
-const {multer,storage} = require('./middleware/multerConfig') 
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'storage/');
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+    }
+});
+const upload = multer({ storage: storage })
+
 const User = require("./model/userModel")
 const Form = require('./model/formmodel')
-const upload = multer({storage : storage})
 const jwt = require("jsonwebtoken")
 const isAuthenticated = require("./middleware/isAuthenticated")
 
 connectToDb()
 
 app.use(express.json())
-app.use(express.urlencoded({extended : true}))
+app.use(express.urlencoded({ extended: true }))
 
-app.set('view engine','ejs')
+app.set('view engine', 'ejs')
 
-app.get("/", async (req,res)=>{
-    const blogs = await Blog.find() // always returns arrray 
-    res.render("./blog/home",{blogs})
+app.get("/", async (req, res) => {
+    const blogs = await Blog.find()
+    res.render("./blog/home", { blogs })
 })
 
-app.get("/about",isAuthenticated, (req,res)=>{
+app.get("/about", isAuthenticated, (req, res) => {
     const name = "Guru Prakash Gupta"
-    res.render("./blog/about",{name})
+    res.render("./blog/about", { name })
 })
-app.get("/contact",isAuthenticated, (req,res)=>{
-    const data="Please do contact"
-    res.render("./blog/contact",{data})
+
+app.get("/contact", isAuthenticated, (req, res) => {
+    const data = "Please Feel free to contact us"
+    res.render("./blog/contact", { data })
 })
-app.get("/createblog",isAuthenticated, (req,res)=>{
+
+app.get("/createblog", isAuthenticated, (req, res) => {
     console.log(req.userId)
     res.render("./blog/createblog")
 })
+app.get('/blogs', async (req, res) => {
+    try {
+        const blogs = await Blog.find(); // Fetch all blogs from the database
+        res.render('blog/blog', { blogs }); // Render the blog.ejs template with the blogs data
+    } catch (err) {
+        res.status(500).send('Server Error');
+    }
+});
 
-app.post("/submitform", async (req,res)=>{
- 
-    const {name,email,message} = req.body 
-    console.log(name,email,message)
+app.post("/submitform", async (req, res) => {
+    const { name, email, message } = req.body
+    console.log(name, email, message)
 
-   await Form.create({
-        name, 
-        email , 
-        message, 
-    })
-
+    await Form.create({ name, email, message })
     res.send("Form submitted")
 })
-app.post("/createblog",upload.single('image') ,async (req,res)=>{
-    // const title = req.body.title 
-    // const subtitle = req.body.subtitle 
-    // const description  = req.body.description 
+
+app.post("/createblog", upload.single('image'), async (req, res) => {
     const fileName = req.file.filename
-    const {title,subtitle,description} = req.body 
-    console.log(title,subtitle,description)
+    const { title, subtitle, description } = req.body
+    console.log(title, subtitle, description)
 
-   await Blog.create({
-        title, 
-        subtitle , 
-        description, 
-        image : fileName
-    })
-
+    await Blog.create({ title, subtitle, description, image: fileName })
     res.send("Blog created successfully")
 })
 
-app.get("/blog/:id",async (req,res)=>{
+app.get("/blog/:id", async (req, res) => {
     const id = req.params.id
     const data = await Blog.findById(id)
-    res.render("./blog/singleblog",{data})
+    res.render("./blog/singleblog", { data })
 })
 
-app.get("/deletedata/:id",async (req,res)=>{
-    const id = req.params.id 
-    const data= await Blog.findByIdAndDelete(id)
+app.get("/deletedata/:id", async (req, res) => {
+    const id = req.params.id
+    await Blog.findByIdAndDelete(id)
     res.redirect("/")
 })
 
-
-app.get("/editdata/:id",async (req,res)=>{
+app.get("/editdata/:id", async (req, res) => {
     const id = req.params.id
-    // const {id} = req.params 
-  const data=   await Blog.findById(id) 
-    res.render("./blog/edit",{data})
+    const data = await Blog.findById(id)
+    res.render("./blog/edit", { data })
 })
 
-app.post("/editdata/:id",async (req,res)=>{
-    const id = req.params.id 
-    const {title,subtitle,description} = req.body 
-    await Blog.findByIdAndUpdate(id,{
-        title : title, 
-        subtitle : subtitle, 
-        description : description
-    })
-    res.redirect("/blog/" + id)
-})
+app.post('/editdata/:id', upload.single('image'), async (req, res) => {
+    const { id } = req.params;
+    const { title, subtitle, description } = req.body;
+    const updateData = { title, subtitle, description };
 
-app.get("/register",(req,res)=>{
+    if (req.file) {
+        const fileName = req.file.filename;
+        updateData.image = fileName;
+    }
+
+    try {
+        await Blog.findByIdAndUpdate(id, updateData);
+        res.redirect(`/blog/${id}`);
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
+
+app.get("/register", (req, res) => {
     res.render("./authentication/register")
 })
-app.get("/login",(req,res)=>{
+
+app.get("/login", (req, res) => {
     res.render("./authentication/login")
 })
 
-app.post("/register",async (req,res)=>{
-    const {username,email,password} = req.body 
-   await User.create({
-        username : username, 
-        email : email, 
-        password : bcrypt.hashSync(password,12)
+app.post("/register", async (req, res) => {
+    const { username, email, password } = req.body
+    await User.create({
+        username: username,
+        email: email,
+        password: bcrypt.hashSync(password, 12)
     })
     res.redirect("/login")
 })
 
-app.post("/login",async (req,res)=>{
-    const {email,password} = req.body 
-  const user = await User.find({email : email})
+app.post("/login", async (req, res) => {
+    const { email, password } = req.body
+    const user = await User.find({ email: email })
 
-  if(user.length
-     === 0){
-    res.send("Invalid email")
-  }else{
-    // check password now 
-    const isMatched = bcrypt.compareSync(password,user[0].password)
-    if(!isMatched){
-        res.send("Invalid password")
-    }else{
-        // require("dotenv").config()
-        
-        const token = jwt.sign({userId : user[0]._id},process.env.SECRET,{
-            expiresIn : '20d'
-        })
-        res.cookie("token",token)
-        res.send("logged in successfully")
+    if (user.length === 0) {
+        res.send("Invalid email")
+    } else {
+        const isMatched = bcrypt.compareSync(password, user[0].password)
+        if (!isMatched) {
+            res.send("Invalid password")
+        } else {
+            const token = jwt.sign({ userId: user[0]._id }, process.env.SECRET, {
+                expiresIn: '20d'
+            })
+            res.cookie("token", token)
+            res.send("logged in successfully")
+        }
     }
-  }
-
 })
 
 app.use(express.static("./storage"))
 app.use(express.static("./public"))
 
-app.listen(3011,()=>{
-    console.log("Nodejs project has started at port" + 3011)
+app.listen(3011, () => {
+    console.log("Nodejs project has started at port " + 3011)
 })
